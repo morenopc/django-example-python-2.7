e380ad59d603e99aab46302b851c49621672d1ac
Moreno <moreno.pinheiro@gmail.com>
2013-07-23 23:07:32 -0300

    add django 1.5.1 welcome project

--- setup.py
+++ setup.py
@@ -1,15 +1,17 @@
-from setuptools import setup
+import os
+from setuptools import setup, find_packages
 
-setup(name='YourAppName', version='1.0',
-      description='OpenShift Python-2.7 Community Cartridge based application',
-      author='Your Name', author_email='ramr@example.org',
-      url='http://www.python.org/sigs/distutils-sig/',
+PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
 
-      #  Uncomment one or more lines below in the install_requires section
-      #  for the specific client drivers/modules your application needs.
-      install_requires=['greenlet', 'gevent',
-                        #  'MySQL-python',
-                        #  'pymongo',
-                        #  'psycopg2',
-      ],
-     )
+setup(
+    name='Python27', version='1.0.1',
+    description='OpenShift Python-2.7 Community Cartridge based application',
+    author='Moreno Cunha', author_email='moreno.pinheiro@gmail.com',
+    url='https://github.com/morenopc/django-example-python-2.7',
+    # Instalation setup
+    packages=find_packages(),
+    include_package_data=True,
+    install_requires=open(
+        '%swsgi/openshift/requirements.txt' % os.environ.get(
+            'OPENSHIFT_REPO_DIR', PROJECT_ROOT)).readlines(),
+)
